d7637812aaaffbfbc50b95efa85cfc6c85fcbc21
aBaO <abao883@gmail.com>
2020-05-16 21:02:18 +0800

    表情反应数据

---
+++ database/reactions.js
@@ -0,0 +1,274 @@
+[
+  {
+    "engName": "Aggravation",
+    "name": "生气",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Outraged.png",
+    "source": "元气"
+  },
+  {
+    "engName": "Agreement",
+    "name": "点头",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Nodding.png",
+    "source": "暴躁"
+  },
+  {
+    "engName": "Amazed",
+    "name": "惊！",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Shocked.png",
+    "source": "成熟"
+  },
+  {
+    "engName": "Apologetic",
+    "name": "鞠躬",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Apologize.png",
+    "source": "大姐姐"
+  },
+  {
+    "engName": "Bashfulness",
+    "name": "害羞",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Blushing.png",
+    "source": "悠闲"
+  },
+  {
+    "engName": "Bewilderment",
+    "name": "嗯？",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Pardon.png",
+    "source": "暴躁"
+  },
+  {
+    "engName": "Cold Chill",
+    "name": "咻～",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/ColdChill.png",
+    "source": "大姐姐"
+  },
+  {
+    "engName": "Confident",
+    "name": "交给我",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Assent.png",
+    "source": "大姐姐",
+    "notes": "Requires a high level of friendship"
+  },
+  {
+    "engName": "Curiosity",
+    "name": "什么？",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/QuestionMark.png",
+    "source": "元气"
+  },
+  {
+    "engName": "Daydreaming",
+    "name": "想象中",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/AbsentMindedness.png",
+    "source": "普通",
+    "notes": "Requires a high level of friendship"
+  },
+  {
+    "engName": "Delight",
+    "name": "拍手",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Clapping.png",
+    "source": "All"
+  },
+  {
+    "engName": "Disagreement",
+    "name": "不行不行",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Negative.png",
+    "source": "大姐姐"
+  },
+  {
+    "engName": "Distress",
+    "name": "紧张",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Frantic.png",
+    "source": "运动"
+  },
+  {
+    "engName": "Dozing",
+    "name": "睡着了",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Sleep.png",
+    "source": "自恋"
+  },
+  {
+    "engName": "Encouraging",
+    "name": "加油～！",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Cheering.png",
+    "source": "运动"
+  },
+  {
+    "engName": "Fearful",
+    "name": "瑟瑟发抖",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Shaking.png",
+    "source": "普通"
+  },
+  {
+    "engName": "Flourish",
+    "name": "舞动",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Dance.png",
+    "source": "运动",
+    "notes": "Requires a high level of friendship"
+  },
+  {
+    "engName": "Glee",
+    "name": "兴高采烈",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/HappyDance.png",
+    "source": "普通"
+  },
+  {
+    "engName": "Greetings",
+    "name": "你好！",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Greeting.png",
+    "source": "All"
+  },
+  {
+    "engName": "Happiness",
+    "name": "笑眯眯",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Smiling.png",
+    "source": "元气"
+  },
+  {
+    "engName": "Heartbreak",
+    "name": "心碎",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/BrokenHeart.png",
+    "source": "自恋"
+  },
+  {
+    "engName": "Inspiration",
+    "name": "灵光一闪",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/IdeaBulb.png",
+    "source": "暴躁",
+    "notes": "Requires a high level of friendship"
+  },
+  {
+    "engName": "Intense",
+    "name": "瞪",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Silent.png",
+    "source": "成熟"
+  },
+  {
+    "engName": "Joy",
+    "name": "开心",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/HappyFlower.png",
+    "source": "All"
+  },
+  {
+    "engName": "Laughter",
+    "name": "笑哈哈",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Laughing.png",
+    "source": "大姐姐"
+  },
+  {
+    "engName": "Love",
+    "name": "喜欢",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Love.png",
+    "source": "成熟",
+    "notes": "Requires a high level of friendship"
+  },
+  {
+    "engName": "Mischief",
+    "name": "坏笑",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Scheming.png",
+    "source": "元气",
+    "notes": "Requires a high level of friendship"
+  },
+  {
+    "engName": "Mistaken",
+    "name": "糟糕！",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Oops.png",
+    "source": "悠闲"
+  },
+  {
+    "engName": "Pleased",
+    "name": "对吧！",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Hello.png",
+    "source": "普通"
+  },
+  {
+    "engName": "Pride",
+    "name": "得意！",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/SmugFace.png",
+    "source": "悠闲",
+    "notes": "Requires a high level of friendship"
+  },
+  {
+    "engName": "Resignation",
+    "name": "好了好了",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/OhGeez.png",
+    "source": "自恋"
+  },
+  {
+    "engName": "Sadness",
+    "name": "失落",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/SadSpiral.png",
+    "source": "普通"
+  },
+  {
+    "engName": "Sheepishness",
+    "name": "苦笑",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/WrySmile.png",
+    "source": "暴躁"
+  },
+  {
+    "engName": "Shocked",
+    "name": "吓一跳！",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Surprised.png",
+    "source": "运动"
+  },
+  {
+    "engName": "Showmanship",
+    "name": "看我的",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Shaki.png",
+    "source": "自恋",
+    "notes": "Requires a high level of friendship"
+  },
+  {
+    "engName": "Shyness",
+    "name": "不好意思",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Hesitate.png",
+    "source": "悠闲"
+  },
+  {
+    "engName": "Sighing",
+    "name": "叹气",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Sighing.png",
+    "source": "成熟"
+  },
+  {
+    "engName": "Sleepy",
+    "name": "打哈欠",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Sleepy.png",
+    "source": "元气"
+  },
+  {
+    "engName": "Smirking",
+    "name": "微笑",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Grin.png",
+    "source": "自恋"
+  },
+  {
+    "engName": "Sneezing",
+    "name": "阿嚏",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Sneezing.png",
+    "source": "运动"
+  },
+  {
+    "engName": "Sorrow",
+    "name": "大哭",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Crying.png",
+    "source": "悠闲"
+  },
+  {
+    "engName": "Surprise",
+    "name": "啊！",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Aha.png",
+    "source": "All"
+  },
+  {
+    "engName": "Thought",
+    "name": "思考",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Thinking.png",
+    "source": "成熟"
+  },
+  {
+    "engName": "Worry",
+    "name": "烦恼",
+    "image": "https://acnhcdn.com/latest/ManpuIcon/Worried.png",
+    "source": "暴躁"
+  }
+]
